f5ac711fb43df30fbaf3e5bf501c033efda48bef
utiyama <ninjin0604@gmail.com>
2024-03-23 17:06:29 +0900

    読み込むSVGを最小限にする

--- workspaces/app/src/pages/BookDetailPage/internal/FavButton.tsx
+++ workspaces/app/src/pages/BookDetailPage/internal/FavButton.tsx
@@ -1,6 +1,7 @@
+import Favorite from '@mui/icons-material/Favorite';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 import styled from 'styled-components';
 
-import { SvgIcon } from '../../../features/icons/components/SvgIcon';
 import { Color, Space } from '../../../foundation/styles/variables';
 
 const _Button = styled.button<{ $outlined: boolean }>`
@@ -28,12 +29,11 @@ export const FavButton: React.FC<Props> = ({ enabled, onClick }) => {
       aria-label={enabled ? 'お気に入りを解除する' : 'お気に入りに追加する'}
       onClick={onClick}
     >
-      <SvgIcon
-        color={enabled ? Color.Favorite : Color.MONO_40}
-        height={24}
-        type={enabled ? 'Favorite' : 'FavoriteBorder'}
-        width={24}
-      />
+      {enabled ? (
+        <Favorite style={{ color: Color.Favorite, height: 24, width: 24 }} />
+      ) : (
+        <FavoriteBorder style={{ color: Color.MONO_40, height: 24, width: 24 }} />
+      )}
     </_Button>
   );
 };
